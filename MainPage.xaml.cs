e4559b9cdbd3726804505eb9425627607f817c06
Jinyeob <jinyeob07@gmail.com>
2020-09-10 16:04:55 +0900

    Add Manna Contents

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.IO;
 using System.Text;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace htmlparsing
 {
@@ -20,6 +21,7 @@ namespace htmlparsing
 
             _id = id;
             _passwd = passwd;
+            dosomething1();
             dosomething();
         }
         public static CookieContainer cookie = new CookieContainer();
@@ -27,12 +29,22 @@ namespace htmlparsing
 
         void Button_Clicked(System.Object sender, System.EventArgs e)
         {
-            //Contents contents = new Contents();
-            // string content = loginImageURL(mainLogin());
-            // label.Text = getText(content);
-             dosomething();
+            dosomething1();
+            dosomething();
         }
-         public void dosomething()
+        public void dosomething1()
+        {
+            string contents = GetMannaText(mainLogin());
+            if (contents == "quit")
+            {
+                BackToLogin();
+            }
+            else
+            {
+                MannaText.Text = contents;
+            }
+        }
+        public void dosomething()
         {
             string checkUrl = getMannaImage(mainLogin());
             if (checkUrl == "quit")
@@ -42,11 +54,9 @@ namespace htmlparsing
             else
             {
                 image.Source = checkUrl;
-                image.Aspect = Aspect.AspectFit;
-                image.HeightRequest = 500;
             }
         }
-
+        
         public HttpWebResponse mainLogin()
         {
             HttpWebRequest req = (HttpWebRequest)WebRequest.Create("https://community.jbch.org/confirm.php");
@@ -72,7 +82,7 @@ namespace htmlparsing
             return resp;
         }
 
-        public string loginImageURL(HttpWebResponse resp)
+        public string GetMannaText(HttpWebResponse resp)
         {
             string htmlBuffer = "";
             HttpWebResponse respBuffer = resp;
@@ -90,49 +100,70 @@ namespace htmlparsing
 
              var getAttr = htmlDoc.DocumentNode.SelectSingleNode("//div[@class='conbox active']/div[@onclick]");
 
+            if (getAttr == null)
+            {
+                return "quit";
+            }
+            else
+            {
+                var getURL = getAttr.GetAttributeValue("onclick", string.Empty);
 
-            var getURL = getAttr.GetAttributeValue("onclick", string.Empty);
-            
-            string thumUrlString = getURL
-                            .Replace("getUrl('", "")
-                            .Replace("', '')", "");
+                string thumUrlString = getURL
+                                .Replace("getUrl('", "")
+                                .Replace("', '')", "");
 
-           // Console.WriteLine("thumUrlString : " + thumUrlString);
+                // Console.WriteLine("thumUrlString : " + thumUrlString);
 
-            String target = "?uid=";
-            int target_num = thumUrlString.IndexOf(target);
-            int target2_num = thumUrlString.IndexOf("&");
-          //  Console.WriteLine("target_num : "+target_num);
-         //   Console.WriteLine("target2_num : " + target2_num);
+                String target = "?uid=";
+                int target_num = thumUrlString.IndexOf(target);
+                int target2_num = thumUrlString.IndexOf("&");
+                //  Console.WriteLine("target_num : "+target_num);
+                //   Console.WriteLine("target2_num : " + target2_num);
 
-            String result;
-           result = thumUrlString.Substring(target_num + 5, target2_num-target_num-5);
+                String result;
+                result = thumUrlString.Substring(target_num + 5, target2_num - target_num - 5);
 
-         //  Console.WriteLine(result);
-
-            
-            ////////////////////////////////////////////////
-
-            string htmlBuffer2 = "";
-            HttpWebResponse respBuffer2 = resp;
-            HttpWebRequest req2 = (HttpWebRequest)WebRequest.Create("http://community.jbch.org/meditation/board/process.php");
-
-            req2.CookieContainer = new CookieContainer();
-            req2.CookieContainer.Add(respBuffer2.Cookies);
-
-            string str = "mode=load_post&post_uid=" + result;
-            TextWriter w2 = (TextWriter)new System.IO.StreamWriter(req2.GetRequestStream());
-
-            w2.Write(str);
-            w2.Close();
-
-            respBuffer2 = (HttpWebResponse)req2.GetResponse();
-            TextReader r2 = (TextReader)new StreamReader(respBuffer2.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
-            htmlBuffer2 = r2.ReadToEnd();
-            
+                //  Console.WriteLine(result);
 
 
-            return htmlBuffer;
+                ////////////////////////////////////////////////
+
+                string htmlBuffer2 = "";
+                HttpWebResponse respBuffer2 = resp;
+                string str = "mode=load_post&post_uid=" + result;
+
+                HttpWebRequest req2 = (HttpWebRequest)WebRequest.Create("http://community.jbch.org/meditation/board/process.php/");
+                req2.Method = "Post";
+
+                req2.CookieContainer = new CookieContainer();
+                req2.CookieContainer.Add(respBuffer2.Cookies);
+
+                req2.ContentLength = str.Length;
+                req2.ContentType = "application/x-www-form-urlencoded; charset=utf-8";
+
+                TextWriter w2 = (TextWriter)new System.IO.StreamWriter(req2.GetRequestStream());
+
+                w2.Write(str);
+                w2.Close();
+
+                respBuffer2 = (HttpWebResponse)req2.GetResponse();
+                TextReader r2 = (TextReader)new StreamReader(respBuffer2.GetResponseStream(), Encoding.GetEncoding("UTF-8"));
+                htmlBuffer2 = r2.ReadToEnd();
+
+                var htmlDoc2 = new HtmlDocument();
+                htmlDoc2.LoadHtml(htmlBuffer2);
+
+                var tmp = htmlDoc2.DocumentNode.SelectSingleNode("//div[@class='contentbox fr-view']/p").InnerHtml;
+
+                tmp = Regex.Replace(tmp, @"<br>", "\n\n");
+
+                // Console.WriteLine(htmlBuffer2);
+
+
+
+
+                return tmp;
+            }
         }
 
 
--- obj/Debug/netstandard2.0/MainPage.xaml.g.cs
+++ obj/Debug/netstandard2.0/MainPage.xaml.g.cs
@@ -20,9 +20,13 @@ namespace htmlparsing {
         private global::Xamarin.Forms.Image image;
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Label MannaText;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(MainPage));
             image = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Image>(this, "image");
+            MannaText = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "MannaText");
         }
     }
 }
