89717b6f0189129c399fa59f67813db48ce81102
Jinyeob <jinyeob07@gmail.com>
2020-09-10 13:07:27 +0900

    Update UI, Save Login Info, Login Check, Fix All Bugs

---
+++ CheckAuth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace htmlparsing
+{
+    public class CheckAuth : ContentPage
+    {
+        public CheckAuth()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
