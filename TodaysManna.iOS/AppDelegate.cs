96b43a7a1d350e22cf87af758da2d7efe9cc1bd5
Jinyeob <jinyeob07@gmail.com>
2020-10-07 18:51:37 +0900

    Update McChain WebView

--- TodaysManna.iOS/AppDelegate.cs
+++ TodaysManna.iOS/AppDelegate.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using Foundation;
 using UIKit;
 
@@ -24,6 +23,7 @@ namespace TodaysManna.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
+            Xam.Plugin.WebView.iOS.FormsWebViewRenderer.Initialize();
 
             return base.FinishedLaunching(app, options);
         }
