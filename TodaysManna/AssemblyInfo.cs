aec28ea4e57dd1cbc3da05d16e48a2133f0786e8
nathankim0 <jinyeob07@gmail.com>
2021-02-22 01:05:33 +0900

    Add FontImage

--- TodaysManna/AssemblyInfo.cs
+++ TodaysManna/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 [assembly: ExportFont("dotum.ttf")]
+[assembly: ExportFont("materialdesignicons.ttf", Alias = "materialdesignicons")]
