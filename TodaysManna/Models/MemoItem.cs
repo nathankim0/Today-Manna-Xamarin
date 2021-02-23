f56b96d0c016e177134bf40eea92971e3337acc6
nathan9608 <nathan.kim@dreamfora.com>
2021-02-23 18:51:29 +0900

    Add Date property

--- TodaysManna/Models/MemoItem.cs
+++ TodaysManna/Models/MemoItem.cs
@@ -1,10 +1,12 @@
-﻿using SQLite;
+﻿using System;
+using SQLite;
 namespace TodaysManna.Models
 {
     public class MemoItem
     {
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
+        public DateTime Date { get; set; }
         public string Verse { get; set; }
         public string Note { get; set; }
     }
