e5b2708be5366b7ba4a68566966f7350151946bd
nathankim0 <jinyeob07@gmail.com>
2021-09-13 01:34:03 +0900

    feat: HomePage 생성, 만나 글씨 바인딩 까지 작업

---
+++ TodaysManna/ViewsV2/HomePage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using TodaysManna.ViewModelV2;
+
+namespace TodaysManna.ViewsV2
+{
+    public partial class HomePage : BaseContentPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+            BindingContext = new HomePageViewModel();
+        }
+    }
+}
