7684344258d3471662b3c5f8d66d2cbdf82be035
nathankim0 <jinyeob07@gmail.com>
2022-02-02 10:27:01 +0900

    fix: android icon 수정

--- TodaysManna.Android/MainActivity.cs
+++ TodaysManna.Android/MainActivity.cs
@@ -1,6 +1,5 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.Views;
 using Android.OS;
 using Xamarin.Forms;
 using Plugin.FirebasePushNotification;
@@ -9,14 +8,11 @@ using Android.Content;
 
 namespace TodaysManna.Droid
 {
-    [Activity(Label = "오늘의 만나", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
+    [Activity(Label = "오늘의 만나", Icon = "@mipmap/ic_launcher", RoundIcon = "@mipmap/ic_launcher_round", Theme = "@style/MainTheme", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            //TabLayoutResource = Resource.Layout.Tabbar;
-            //ToolbarResource = Resource.Layout.Toolbar;
-
             StatusBar.Activity = this;
 
             base.OnCreate(savedInstanceState);
