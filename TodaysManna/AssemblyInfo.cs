34c4cdba2defc6fd7b376e844ac36ca9090eff78
nathankim0 <jinyeob07@gmail.com>
2022-02-04 09:49:36 +0900

    fix: fonts

--- TodaysManna/AssemblyInfo.cs
+++ TodaysManna/AssemblyInfo.cs
@@ -2,6 +2,10 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
 [assembly: ExportFont("dotum.ttf")]
 [assembly: ExportFont("batang.otf", Alias = "batang")]
 [assembly: ExportFont("materialdesignicons.ttf", Alias = "materialdesignicons")]
+[assembly: ExportFont("Pretendard-Bold.ttf", Alias = "Pretendard-Bold")]
+[assembly: ExportFont("Pretendard-Medium.ttf", Alias = "Pretendard-Medium")]
+[assembly: ExportFont("Pretendard-Regular.ttf", Alias = "Pretendard-Regular")]
